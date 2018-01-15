98b7f1cd9fbe29ebc921f6e63b0832d26c58888c
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-15 12:18:53 +0545

    each event moved to each function.

--- js/carousel.js
+++ js/carousel.js
@@ -1,38 +1,41 @@
 $(document).ready(function(){
     
     $('.next-btn').on('click', function(){
+        var sliderSelecter = '.mySlides';
+        slideNext(sliderSelecter);
+    });
+
+    $('.prev-btn').on('click', function() {
+        var sliderSelecter = '.mySlides';
+        slidePrev(sliderSelecter);
+    });
+
+    setInterval(function(){
+        var sliderSelecter = '.mySlides';
+        slideNext(sliderSelecter);
+    }, 3000);
+
+    function slideNext(sliderSelecter) {
         var currentSlide = $('.active');
-        var nextSlide = currentSlide.next('.mySlides');
+        var nextSlide = currentSlide.next(sliderSelecter);
         if (nextSlide.length) {
         	nextSlide.addClass('active');    
         	currentSlide.removeClass('active');	
         } else {
-            $('.mySlides').first().addClass('active');    
+            $(sliderSelecter).first().addClass('active');    
         	currentSlide.removeClass('active');
         }
-    });
+    }
 
-    $('.prev-btn').on('click', function() {
+    function slidePrev(sliderSelecter) {
         var currentSlide = $('.active');
-        var prevSlide = currentSlide.prev('.mySlides');
+        var prevSlide = currentSlide.prev(sliderSelecter);
         if (prevSlide.length) {
         	prevSlide.addClass('active');
         	currentSlide.removeClass('active');
         } else {
-            $('.mySlides').last().addClass('active');    
+            $(sliderSelecter).last().addClass('active');    
         	currentSlide.removeClass('active');
         }
-    });
-
-    setInterval(function(){
-        var currentSlide = $('.active');
-        var nextSlide = currentSlide.next('.mySlides');
-        if (nextSlide.length) {
-        	nextSlide.addClass('active');    
-        	currentSlide.removeClass('active');	
-        } else {
-            $('.mySlides').first().addClass('active');    
-        	currentSlide.removeClass('active');
-        }
-    }, 3000);
+    }
 });
