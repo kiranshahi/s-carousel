9cc0f842fd89618eebfbf19dd29fbf4fb44d6ef7
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-15 12:02:27 +0545

    Slide on button click done

---
+++ js/carousel.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $('.next-btn').on('click', function(){
+        var currentSlide = $('.active');
+        var nextSlide = currentSlide.next('.mySlides');
+        if (nextSlide.length) {
+        	nextSlide.addClass('active');    
+        	currentSlide.removeClass('active');	
+        } else {
+            $('.mySlides').first().addClass('active');    
+        	currentSlide.removeClass('active');
+        }
+    });
+
+    $('.prev-btn').on('click', function() {
+        var currentSlide = $('.active');
+        var prevSlide = currentSlide.prev('.mySlides');
+        if (prevSlide.length) {
+        	prevSlide.addClass('active');
+        	currentSlide.removeClass('active');
+        } else {
+            $('.mySlides').last().addClass('active');    
+        	currentSlide.removeClass('active');
+        }
+    });
+
+});
