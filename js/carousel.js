475bc5f99748f9a6130c8ed681afc71f949d565f
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-16 14:56:19 +0545

    Converted into jquery plugin

--- js/carousel.js
+++ js/carousel.js
@@ -1,58 +1,58 @@
-$(document).ready(function () {
+(function ($) {
 
-    var interval;
+    $.fn.scarousel = function () {
+        var settings = $.extend({
+            sliderWrapper: '.mySlides'
+        });
+        var interval;
 
-    var container = $('.slideshow-container');
-    
-    startSlider();
+        startSlider();
 
-    $('.next-btn').on('click', function () {
-        var sliderSelecter = '.mySlides';
-        slideNext(sliderSelecter);
-    });
+        $('.next-btn').on('click', function () {
+            slideNext();
+        });
 
-    $('.prev-btn').on('click', function () {
-        var sliderSelecter = '.mySlides';
-        slidePrev(sliderSelecter);
-    });
+        $('.prev-btn').on('click', function () {
+            slidePrev();
+        });
 
-    function slideNext(sliderSelecter) {
-        var currentSlide = $('.active');
-        var nextSlide = currentSlide.next(sliderSelecter);
-        if (nextSlide.length) {
-            nextSlide.addClass('active');
-            currentSlide.removeClass('active');
-        } else {
-            $(sliderSelecter).first().addClass('active');
-            currentSlide.removeClass('active');
+        function slideNext() {
+            var currentSlide = this.$('.active');
+            var nextSlide = currentSlide.next(settings.sliderWrapper);
+            if (nextSlide.length) {
+                nextSlide.addClass('active');
+                currentSlide.removeClass('active');
+            } else {
+                $(settings.sliderWrapper).first().addClass('active');
+                currentSlide.removeClass('active');
+            }
         }
-    }
 
-    function slidePrev(sliderSelecter) {
-        var currentSlide = $('.active');
-        var prevSlide = currentSlide.prev(sliderSelecter);
-        if (prevSlide.length) {
-            prevSlide.addClass('active');
-            currentSlide.removeClass('active');
-        } else {
-            $(sliderSelecter).last().addClass('active');
-            currentSlide.removeClass('active');
+        function slidePrev() {
+            var currentSlide = this.$('.active');
+            var prevSlide = currentSlide.prev(settings.sliderWrapper);
+            if (prevSlide.length) {
+                prevSlide.addClass('active');
+                currentSlide.removeClass('active');
+            } else {
+                $(settings.sliderWrapper).last().addClass('active');
+                currentSlide.removeClass('active');
+            }
         }
-    }
 
-    function startSlider() {
-        interval = setInterval(function () {
-            var sliderSelecter = '.mySlides';
-            slideNext(sliderSelecter);
-        }, 3000);
-    }
+        function startSlider() {
+            interval = setInterval(function () {
+                slideNext();
+            }, 3000);
+        }
 
-    function pauseSlider() {
-        clearInterval(interval);
-    }
+        function pauseSlider() {
+            clearInterval(interval);
+        }
 
-    container
-    .on('mouseenter', pauseSlider)
-	.on('mouseleave', startSlider);
+        this
+            .on('mouseenter', pauseSlider)
+            .on('mouseleave', startSlider);
+    };
 
-});
+}(jQuery));
